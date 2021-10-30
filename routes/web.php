238a798f963ee27820c410ce1198ce2d5aacb05d
Mohammad Prince <mprince2k16@gmail.com>
2021-10-30 18:06:46 +0600

    artisan

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/artisan', function () {
+    Artisan::call('domain:add prince.com');
+    return 'ok';
+});
